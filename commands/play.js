e2618d7ba9fb679b6088341cfc9ee237429f7cb6
Salypse <brandonseitz05@gmail.com>
2021-05-18 11:13:03 -0500

    fdfdfd

--- commands/play.js
+++ commands/play.js
@@ -4,7 +4,8 @@ const  Discord = require('discord.js')
 
 const queue =new Map();
 
-module.exports.run = async (bot,message,args) => {
+module.exports.run = { 
+    async execute(bot,message,args){
 
         const voice_channel = message.member.voice.channel;
         if(!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
@@ -62,6 +63,7 @@ module.exports.run = async (bot,message,args) => {
             }
         }
     }
+}
 
 const video_player = async (guild, song) => {
     const song_queue = queue.get(guild.id);
