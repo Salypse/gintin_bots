70239c1c7534d2368cdd40def7f80c7567f3080b
Salypse <brandonseitz05@gmail.com>
2021-05-18 14:47:01 -0500

    fbdfrgr

--- commands/play.js
+++ commands/play.js
@@ -11,5 +11,5 @@ module.exports.run = async (bot,message,args) => {
 
 
 exports.help = {
-    name: 'mute'
+    name: 'play'
 }
--- commands/skip.js
+++ commands/skip.js
@@ -3,9 +3,9 @@ module.exports.run = async (bot,message,args) => {
     if(!message.member.voice.channel) return message.channel.send("Please join a voice channel first!")
 
     client.player.skip(message);
-    
+
     }
 
 exports.help = {
-    name: 'mute'
+    name: 'play'
 }
