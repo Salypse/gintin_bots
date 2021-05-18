27ac381b2e7b185021404b4cd9a14cf67e3388a9
Salypse <brandonseitz05@gmail.com>
2021-05-18 12:06:01 -0500

    ffbfbfb

--- commands/mute.js
+++ commands/mute.js
@@ -1,22 +1,24 @@
-const ms =require("ms")
+const ms = require ("ms");
 
-module.exports.run = async (bot,message,args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No permission!")
+exports.run = async (bot,message,args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No permission to mute someone!")
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    if(!member) return message.channel.send('Invaled User Provided!')
+    if(!member) return message.channel.send('Invalid User provided!');
     let muteTime = args[1];
     if(!muteTime) return message.channel.send("No time provided");
+
     let msTime = ms(muteTime);
-    let muteRole = message.guild.roles.cache.find(r => r.name == "Muted");
-    if (!muteRole) return message.channel.send("Mute role not found");
+    let muteRole = message.guild.roles.cache.find(r => r.name == "Mute");
+    if(!muteRole) return message.channel.send("Mute role not found!")
     member.roles.add(muteRole);
     message.channel.send("User has been muted!");
 
     setTimeout(() => {
         member.roles.remove(muteRole);
-        message.channel.send("User has been unmuted!")
+        message.channel.send("User has been unmuted")
     }, msTime)
 }
+
 exports.help = {
-    name: 'mute'
+    name: 'Mute',  
 }
